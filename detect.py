4a7d4ef418645b997ca8c4ecbe52e48595336a73
artsun <parafo@gmail.com>
2021-02-11 18:20:09 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,6 +4,19 @@ from subprocess import Popen, PIPE
 from argparse import ArgumentParser
 
 
+def gen_stat(txt: dict):
+    for k in txt.keys():
+        yield (k, len(txt[k]))
+
+
+def stat(fname: str):
+    try:
+        with open(fname, 'r') as f:
+            return gen_stat(json.loads(f.read()))
+    except OSError as er:
+        raise SystemExit(er)
+
+
 def gen(collection: list):
     for el in collection:
         el = el.strip()
@@ -31,7 +44,6 @@ def tree(rpmname: str) -> dict:
     [check_file(res, item) for item in rpm_files]
     return res
 
-
 NAME = ''
 DESCRIPTION = ''
 VERSION = 0.1
@@ -39,12 +51,14 @@ VERSION = 0.1
 if __name__ == '__main__':
     parser = ArgumentParser(prog=NAME, usage=DESCRIPTION, add_help=False, allow_abbrev=False)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-v', action='store_true', help='version')
-    group.add_argument('-h', action='help', help='about')
-    group.add_argument('-p', nargs='?', type=str, action='store', metavar='', help='package name')
+    group.add_argument('-v', action='store_true', help='версия программы')
+    group.add_argument('-h', action='help', help='справка о работе программы')
+    group.add_argument('-p', nargs='?', type=str, action='store', metavar='', help='имя пакета')
+    group.add_argument('-stat', nargs='?', type=str, action='store', metavar='', help='файла')
 
     opt = parser.add_argument_group()
-    opt.add_argument('-f', dest='file_save', nargs='?', type=str, help='save to file', metavar='')
+    opt.add_argument('-f', dest='file_save', nargs='?', type=str, help='сохранить в файл с заданным именем', metavar='')
+    opt.add_argument('-s', dest='file_same', action='store_true', help='сохранить в файл с именем {rpm}.json')
 
     args = parser.parse_args()
     if args.v:
@@ -53,6 +67,13 @@ if __name__ == '__main__':
         if args.file_save:
             with open(args.file_save, 'w') as f:
                 f.write(json.dumps(tree(args.p), indent=4, sort_keys=True))
-        else:
+        elif args.file_same:
+            with open(f'{args.p}.json', 'w') as f:
+                f.write(json.dumps(tree(args.p), indent=4, sort_keys=True))
+        else:               
             print(json.dumps(tree(args.p), indent=4, sort_keys=True))
+    if args.stat:
+        print('statistics')
+        [print(x) for x in stat(args.stat)]
     raise SystemExit()
+
